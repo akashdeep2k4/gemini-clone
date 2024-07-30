48b4ddae7e91706c1f69b1cd201cef2c822a7fc0
Akashdeep <akashdeep2k4@gmail.com>
2024-07-30 21:43:54 +0530

    commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
 import "./Main.css";
+import { Context } from "../../context/Context";
 
 export default function Main() {
+    const { onSent, recentPrompts, showResult, loading, resultData, setInput, input } = useContext(Context);
+
     return (
         <div className="main">
             <div className="nav">
@@ -35,10 +39,10 @@ export default function Main() {
                 </div>
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder="Enter a prompt here" />
+                        <input onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+                        <img onClick={() => onSent()} src={assets.send_icon} alt="" />
                     </div>
                     <p className="bottom-info">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
                 </div>
